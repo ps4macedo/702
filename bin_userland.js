8082fdae6c458dfb09907d018f3e237bcfdef60a
ps4macedo <69283643+ps4macedo@users.noreply.github.com>
2021-01-15 19:20:15 -0300

    Add files via upload

--- bin_userland.js
+++ bin_userland.js
@@ -290,8 +290,7 @@ function stage2() {
       new_thr.fcall(libKernelBase.add32(OFFSET_lk_pthread_exit), 0);
     });
     loader_thr();
-	alert("P R O N T O ! ! ! Sua carga útel já pode ser enviada. \n\nUtilize o ip do console e a porta 9020. \nLink do aplicativo para enviar as cargas úteis:\n https://github.com/ps4macedo/702/raw/main/apps/ps4punch.rar \nLink do WebRTE: http://ps4trainer.com/Trainer/WebRTE_702.bin \n    Obs.: Para sair, pressione o \"OPTION\" e selecione \"Fechar janela\".");
-  
+	alert("P R O N T O ! ! ! Sua carga útel já pode ser enviada. \n\n A T E N Ç Ã O !!! O computador deve estar na mesma rede do PS4.\nUtilize o ip do console e a porta 9020. \nLink do aplicativo para enviar as cargas úteis:\n https://github.com/ps4macedo/702/raw/main/apps/ps4punch.rar \n    Obs.: Para sair, pressione o \"OPTION\" e selecione \"Fechar janela\"."); 
 } 
 
 function stage3() {
