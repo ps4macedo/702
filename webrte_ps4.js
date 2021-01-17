be9a0b35a19225dc272ce910fc439a224c1796e7
ps4macedo <69283643+ps4macedo@users.noreply.github.com>
2021-01-17 20:11:29 -0300

    Add files via upload

--- webrte_ps4.js
+++ webrte_ps4.js
@@ -123,7 +123,7 @@ function setupRW() {
 	debug_log("ATENÇÃO PARA NÃO PERDER A CACHE OFFLINE!!!");
 	debug_log("Para \"SAIR\", pressione o botão \"OPTION\", selecione \"Fechar janela\",");
 	debug_log("\"OK\" na mensagem de memória insuficente, selecione a \"JANELA PRINCIPAL\".");
-	debug_log("e, por fim, saia do nevegador usando o \"CÍRCULO\".");
+	debug_log("e, por fim, saia do navegador usando o \"CÍRCULO\".");
 
 	/* Getting code execution */
 	/* ... */
